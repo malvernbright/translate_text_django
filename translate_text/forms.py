ab97bcc4e723f38aa7efe62c3373e1d9bb98ffb2
malvernbright <malvernbright.95@gmail.com>
2023-08-04 15:25:57 +0200

    Added @csrf_protect to forms

--- translate_text/forms.py
+++ translate_text/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.views.decorators.csrf import csrf_protect
 
+@csrf_protect
 class TranslateForm(forms.Form):
     input_text = forms.CharField(label="Text to translate")
